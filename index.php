a1981abcab4e81b03a4601ba70b92540657126bb
dolgov-a <129279819+dolgov-a@users.noreply.github.com>
2023-04-02 18:04:14 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/icons/favicon-16x16.png">
+    <title>Главная</title>
+</head>
+<body>
+        <a href="#" class="scrolltop" id="scroll-top">
+            <i class='bx bxs-chevron-up scrolltop_icon'></i>
+        </a>
+
+
+    <header class="l-header" id="header">
+            <nav class="nav bd-container">
+                <a href="index.html" class="nav-logo"><i class='logo-pic bx bx-camera'></i></a>
+                <div class="nav-menu" id="nav-menu">
+                    <ul class="nav-list">
+                        <li class="nav-item"><a href="#home" class="nav-link">Главная</a></li>
+                        <li class="nav-item"><a href="#me" class="nav-link">Обо мне</a></li>
+                        <li class="nav-item"><a href="#price" class="nav-link">Цена</a></li>
+                        <li class="nav-item"><a href="#send" class="nav-link">Заявка</a></li>
+
+                        <li><i class='bx bx-moon change-theme' id="theme-button"></i></li>
+                    </ul>
+                </div>
+    
+                <div class="nav-toggle" id="nav-toggle">
+                    <i class='bx bx-menu-alt-right'></i>
+                </div>
+            </nav>
+    </header>
+
+
+
+<main class="l-main">
+
+
+
+            <img class="banner" src="assets/img/main-banner.jpg">
+            <h1 class="banner-title">Профессиональная фотосессия от 50 тыс рублей, обработка фото беслпатно!</h1>
+
+
+
+        <section class="home" id="home">
+            <div class="home-container bd-container bd-grid">
+                <div class="home-img">
+                    <img src="assets/img/main-pfp.jpg">
+                </div>
+                
+                <div class="home-txt">
+                    <h1 class="home-title">Для Вас</h1>
+                    <p class="home-desc">Я сохраняю в своих снимках настроение праздника, с его красотой, драйвом, эмоциями и нежностью, считая, что настоящее счастье должно существовать вне времени. 
+
+                        Чтобы спустя годы вы и ваши дети смотрели фотографии и ощущали всё это так, будто оно происходит прямо здесь, сейчас, сегодня!
+                    </p>
+                    <a href="about.html" class="button">&#10148; Обо мне</a>
+                </div>
+            </div>
+        </section>
+
+
+<section class="section">
+
+    <div class="slider bd-grid">
+      
+
+        <div class="item">
+            <img src="assets/img/slide1.jpg">
+        </div>
+      
+
+        <div class="item">
+            <img src="assets/img/slide2.jpg">
+        </div>
+        <div class="item">
+            <img src="assets/img/slide3.jpg">
+        </div>
+
+        <a class="previous" onclick="previousSlide()"><i class='bx bx-chevron-left'></i></a>
+        <a class="next" onclick="nextSlide()"><i class='bx bx-chevron-right' ></i></a>
+        </div>
+        </div>
+    </section>
+
+
+
+                <section class="me section bd-container" id="me">
+                    <div class="me-container bd-grid">
+                        <div class="me-txt">
+                            <h1 class="section-title-center">Обо мне</h1>
+                            <p class="me-desc">Привет, меня зовут Надежда! 
+                                
+                                На своих кадрах я всегда стараюсь передать эстетику вашего праздника, будь то камерная свадьба или большое торжество!
+                                
+                                Я специализируюсь и лучше всего снимаю:
+                                
+                                Свадебные истории / Репортаж / Светскую хронику
+                                
+                                 
+                                </p>
+                        </div>
+        
+                        <div class="me-img">
+                            <img src="assets/img/about-pfp.jpg">
+                        </div>
+                    </div> 
+                </section>
+        
+        
+         <section class="accessory section bd-container">
+            <h2 class="section-title">Моё <br> портфолио</h2>
+        
+        <div class="row"> 
+            <div class="column">
+              <img src="assets/img/portfolio1.jpg" style="width:100%">
+              <img src="assets/img/portfolio2.jpg" style="width:100%">
+              <img src="assets/img/portfolio3.jpg" style="width:100%">
+              <img src="assets/img/portfolio4.jpg" style="width:100%">
+              <img src="assets/img/portfolio5.jpg" style="width:100%">
+        
+            </div>
+            <div class="column">
+              <img src="assets/img/portfolio7.jpg" style="width:100%">
+              <img src="assets/img/portfolio8.jpg" style="width:100%">
+              <img src="assets/img/portfolio9.jpg" style="width:100%">
+              <img src="assets/img/portfolio10.jpg" style="width:100%">
+              <img src="assets/img/portfolio6.jpg" style="width:100%">
+            </div>  
+            <div class="column">
+              <img src="assets/img/portfolio13.jpg" style="width:100%">
+              <img src="assets/img/portfolio14.jpg" style="width:100%">
+              <img src="assets/img/portfolio15.jpg" style="width:100%">
+              <img src="assets/img/portfolio16.jpg" style="width:100%">
+              <img src="assets/img/portfolio20.jpg" style="width:100%">
+            </div>
+            <div class="column">
+                <img src="assets/img/portfolio17.jpg" style="width:100%">
+                <img src="assets/img/portfolio18.jpg" style="width:100%">
+                <img src="assets/img/portfolio19.jpg" style="width:100%">
+                <img src="assets/img/portfolio11.jpg" style="width:100%">
+                <img src="assets/img/portfolio12.jpg" style="width:100%">
+            </div>
+          </div>
+            </div>
+        </section>
+        
+        <section class="banner-img bd-grid" id="price">
+            <img class="banner" src="assets/img/price-banner.jpg">
+    
+            <h2 class="banner-title-price home-container">Всё то, что вы хотите узнать!</h2>
+            <h1 class="banner-desc home-container">
+              Портфолио - только вершина айсберга. За крутыми кадрами стоят те "невидимые" процессы, в которые я вкладываюсь по максимуму силами, временем и средствами, не экономя на важном:
+              
+               
+              
+              первоклассное техническое и программное обеспечение, постоянное повышение квалификации, полное погружение в подготовку и обработку. 
+              
+               
+              
+              Свадебная фотография находится на уровне семейных ценностей, а значит заслуживает лучших решений.
+              
+            </h1>
+    
+          </section>
+    
+    <section class="bd-container">
+        <h1 class="section-title">
+    
+          Какие есть готовые предложения
+          
+          для свадебных вечеринок?
+          </h1>
+        <div class="price-container bd-grid">
+            <div class="pricing-table">
+                <h3 class="pricing-title">Тариф "1"</h3>
+    
+                <ul class="table-list">
+                <li>консультация</span></li>
+                <hr>
+                <li>работа до 4 часов</span></li>
+                <hr>
+                <li>200 + обработанных фото </span></li>
+                <hr>
+                <li>онлайн галерея </span></li>
+                <hr>
+                <li>ассистент ( по необходимости )</span></li>
+                 </ul>  
+            <div class="table-buy">
+              <p>
+                р50.000
+                </p>
+            </div>
+          </div>
+          
+          <div class="pricing-table">
+            <h3 class="pricing-title">Тариф "2"</h3>
+            <ul class="table-list">
+            <li>консультация</span></li>
+            <hr>
+            <li>работа до 10 часов</span></li>
+            <hr>
+            <li>500 + обработанных фото </span></li>
+            <hr>
+            <li>онлайн галерея </span></li> 
+            <hr>
+            <li>ассистент</span></li>
+             </ul>  
+            <div class="table-buy">
+              <p>р80.000</p>
+            </div>
+          </div>
+      
+          <div class="pricing-table">
+            <h3 class="pricing-title">Ultimate</h3>
+            <ul class="table-list">
+              <li>консультация</span></li>
+              <hr>
+              <li>работа до 12 часов</span></li>
+              <hr>
+              <li>600 + обработанных фото </span></li>
+              <hr>
+              <li>онлайн галерея </span></li> 
+              <hr>
+              <li>ассистент</span></li>
+               </ul>
+            <div class="table-buy">
+              <p>р100.000</p>
+            </div>
+          </div>
+        </div>
+    </div>
+    </section>
+    <section class="bd-container">
+      <div class="button-center">
+    
+      </div>
+    </section>
+
+    <section class="send section" id="send">
+        <?php
+        if(!empty($_POST["send"])) {
+            $userName = $_POST["userName"];
+          $userEmail = $_POST["userEmail"];
+            $userMessage = $_POST["userMessage"];
+            $toEmail = "orderphotonadezda@mail.ru";
+          
+            $mailHeaders = "Name: " . $userName .
+            "\r\n Email: ". $userEmail  . 
+            "\r\n Message: " . $userMessage . "\r\n";
+        
+            if(mail($toEmail, $userName, $mailHeaders)) {
+                $message = "Успешно отправленно.";
+            }
+        }
+        ?>
+            <div class="send-container bd-container bd-grid">
+                <div class="send-content">
+                    <h2 class="section-title-center">Связаться со мной</h2>
+        
+                    <form name="contactFormEmail" method="post">
+                    <div class="input-row">
+                        <input type="text" name="userName" required id="userName" class="feedback-input" placeholder="Имя"> 
+                    </div>
+                    <div class="input-row">
+                        <input type="email" name="userEmail" required id="userEmail" class="feedback-input" placeholder="Email"> 
+                    </div>
+                    <div class="input-row">
+                        <textarea name="userMessage" required id="userMessage" class="feedback-input" placeholder="Текст сообщения"></textarea>
+                    </div>
+                        <div class="input-row">
+                            <input type="submit" name="send" class="button-price" value="Отправить"/>
+                            <?php if (! empty($message)) {?>
+                                <div class='success'>
+                                    <strong><?php echo $message; ?>	</strong>
+                                </div>
+                            <?php } ?>
+                         </div>
+                    </form>
+                </div>
+        
+        
+                            <div class="send-img">
+                                <img src="assets/img/sent-pfp.jpg">
+                            </div>
+            </div>
+        </section>
+
+</main>
+<footer class="footer section">
+    <hr class="bd-container">
+    <h4 class="footer-title bd-container bd-grid">Contact</h4>
+    <div class="footer-container bd-container bd-grid">
+        
+        <div class="footer-content">
+            <ul class="footer-list">
+                <li class="footer-contact-li"><a href="mailto:orderphotonadezda@mail.ru" class="footer-contact"><i class='bx bx-envelope ico'></i></a></li>
+            </ul>
+        </div>
+    </div>
+
+    <p class="footer-copy">&#169; 2023 Nadezda Photographer</p>
+</footer>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
